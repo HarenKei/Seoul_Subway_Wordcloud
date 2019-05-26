f570a725af1339fa7095a0344febd89106808c40
[HarenKei] <seungmin1266@gmail.com>
2019-05-26 17:21:02 +0900

    폰트 배민 한니체로 변경, 서울 지하철역 604개 전부 넣음, 꽉찬 클라우드 생성함. 근데 왜 서울역이 가장 크지?

--- API_Parsing.py
+++ API_Parsing.py
@@ -4,7 +4,6 @@ from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 
 sta_list = []
-
 class dataObject:
     def __init__(self, line_num, sub_sta_nm, ride_pasgr_num, alight_pasgr_num): #생성자 호출
         self.line_num = line_num
@@ -12,9 +11,9 @@ class dataObject:
         self.ride_pasgr_num = ride_pasgr_num
         self.alight_pasger_num = alight_pasgr_num
 
-data = [] #list형 data 변수
-r = requests.get("http://openapi.seoul.go.kr:8088/4b44596977686172363670566c7358/xml/CardSubwayStatsNew/1/105/20190101"
-                 ) # r 변수에 requests.get 메소드 호출, 인자로 url 전잘
+data = []
+r = requests.get("http://openapi.seoul.go.kr:8088/(인증키)/xml/CardSubwayStatsNew/1/604/20190101"
+                 )
 html = r.text
 soup = BeautifulSoup(html, 'html.parser')
 codenumber = soup.find_all('row')
@@ -28,7 +27,7 @@ for i in range(0, len(codenumber)):
 
 data.sort(key = lambda object : object.ride_pasgr_num, reverse = True)
 
-for i in range(9):
+for i in range(0, len(codenumber)):
     print(data[i].line_num)
     print(data[i].sub_sta_nm)
     sta_list.append(data[i].sub_sta_nm)
@@ -36,12 +35,15 @@ for i in range(9):
     print(data[i].alight_pasger_num)
     print("----------------------------")
 
+
+
 station = " ".join(sta_list) #list에 있는 값을 공백을 기준으로 문자열로 변환
 
 
 
+
 wc = WordCloud(font_path='/Users/harenkei/PycharmProjects/SeoulAPI_parsing/BMHANNA_11yrs_.ttf',
-               background_color='white', width=800, height=600).generate_from_text(station)
+               background_color='black', width=1920, height=1080).generate_from_text(station)
 
 plt.imshow(wc)
 plt.axis("off")
