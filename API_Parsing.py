38fdae013d70f6e8b44ad5b939ec3fbed4dd38bc
[HarenKei] <seungmin1266@gmail.com>
2019-06-13 13:02:04 +0900

    역 이름과 빈도를 딕셔너리로 저장하여 인자로 전달.

--- API_Parsing.py
+++ API_Parsing.py
@@ -4,6 +4,7 @@ from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 
 sta_list = []
+dic ={}
 class dataObject:
     def __init__(self, line_num, sub_sta_nm, ride_pasgr_num, alight_pasgr_num): #생성자 호출
         self.line_num = line_num
@@ -11,8 +12,10 @@ class dataObject:
         self.ride_pasgr_num = ride_pasgr_num
         self.alight_pasger_num = alight_pasgr_num
 
+
+
 data = []
-r = requests.get("http://openapi.seoul.go.kr:8088/(인증키)/xml/CardSubwayStatsNew/1/604/20190101"
+r = requests.get("http://openapi.seoul.go.kr:8088/4b44596977686172363670566c7358/xml/CardSubwayStatsNew/1/604/20190101"
                  )
 html = r.text
 soup = BeautifulSoup(html, 'html.parser')
@@ -35,15 +38,17 @@ for i in range(0, len(codenumber)):
     print(data[i].alight_pasger_num)
     print("----------------------------")
 
+def MakeToDcit():
+    for i in range(0,len(codenumber)):
+        dic[str(data[i].sub_sta_nm)] = float(data[i].ride_pasgr_num)
+
+MakeToDcit()
 
 
-station = " ".join(sta_list) #list에 있는 값을 공백을 기준으로 문자열로 변환
-
-
-
+#station = " ".join(sta_list) #list에 있는 값을 공백을 기준으로 문자열로 변환
 
 wc = WordCloud(font_path='/Users/harenkei/PycharmProjects/SeoulAPI_parsing/BMHANNA_11yrs_.ttf',
-               background_color='black', width=1920, height=1080).generate_from_text(station)
+               background_color='black', width=1920, height=1080).generate_from_frequencies(dic)
 
 plt.imshow(wc)
 plt.axis("off")
